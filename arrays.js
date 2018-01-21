3b1756a0debac2de5592951207495b6a5300de98
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-21 18:54:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,34 @@ array.unshift(element)
 return (array);  
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
-  array[array.length]= element;
-}
+function addElementToEndOfArray(array, element) {
+ return [...array, element];		 
+   }		  
+  
+  
+  function destructivelyAddElementToEndOfArray(array, element) {		  
+ -  array.push(element);		 +  array.push(element)
+ -  return array;		 +  return array
+ +}
+ +
+ +function destructivelyRemoveElementFromBeginningOfArray(array) {
+ +  array.shift()
+ +  array
+  }		  }
+  		  
+  function removeElementFromBeginningOfArray(array) {		  function removeElementFromBeginningOfArray(array) {
+ -  return array.slice(1);		 +  return array.slice(1)
+  }		  }
+  		  
+  function destructivelyRemoveElementFromEndOfArray(array) {		  function destructivelyRemoveElementFromEndOfArray(array) {
+ -  array.pop();		 +  array.pop()
+ -  return array;		 +  return array
+  }		  }
+  		  
+  function removeElementFromEndOfArray(array) {		  function removeElementFromEndOfArray(array) {
+ -  return array.slice(0, array.length - 1);		 +  return array.slice(0, array.length - 1)
+  }		  }
+  		  
+  function accessElementInArray(array, index) {		  function accessElementInArray(array, index) {
+ -  return array[index];		 +  return array[index]
+  }		  }
